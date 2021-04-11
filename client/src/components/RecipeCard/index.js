61efe06a4f2c78290da45d18b2e9c5052e1b858a
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-04-10 21:21:33 -0400

    updated frontend APIs

--- client/src/components/RecipeCard/index.js
+++ client/src/components/RecipeCard/index.js
@@ -8,20 +8,20 @@ const RecipeCard = (props) => {
 
     return (
         <Col md={12}>
-            <Card className="mb-4 shadow-sm" page={props.page}>
+            <Card className="mb-4 w-60 shadow-sm" page={props.page}>
                 <Row>   
                 <Col md={3}>
                     {
                         (props.thumbnail)
                         ? <Card.Text 
-                            className="text-center"
+                            className="float-left"
                         ><Image 
                         src={props.thumbnail}
                         alt="recipe title" 
                         />
                         </Card.Text>
                         : <Card.Text
-                            className="text-center"
+                            className="float-left"
                         >
                             "https://placehold.it/150-x150"
                         </Card.Text>
@@ -37,7 +37,7 @@ const RecipeCard = (props) => {
                     <Card.Text>
                         {props.ingreedients || "No ingredients listed."}
                     </Card.Text>
-                    <a rel="noreferrer noopener" target="_blank" href={props.href}></a>
+                    <a className="btn-primary float-right" rel="noreferrer noopener" target="_blank" href={props.href}></a>
                     </Col>
                     </Row>
             </Card>
--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -3,13 +3,32 @@ import RecipeCard from "../components/RecipeCard"
 import SearchBar from "../components/SearchBar"
 // import SaveBtn from "../components/DeleteBtn";
 // import Jumbotron from "../components/Jumbotron";
-// import API from "../utils/API";
+import API from "../utils/API";
 // import { Link } from "react-router-dom";
 // import { Col, Row, Container } from "../components/Grid";
 // import { List, ListItem } from "../components/List";
 // import { Input, TextArea, FormBtn } from "../components/Form";
 
 function SearchResults() {
+
+  // Setting our component's initial state
+  const [recipes, setRecipes] = useState([])
+  const [recipeSearch, setRecipeSearch] = useState({})
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadRecipes()
+  }, [])
+
+  // Loads all books and sets them to books
+  function loadRecipes() {
+    API.searchRecipes(recipeSearch)
+      .then(res => 
+        setBooks(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
 
   return(
  <>
@@ -26,3 +45,13 @@ function SearchResults() {
 }
 
 export default SearchResults;
+
+{/* <div class='card' style='width: 600px;'>
+   <div class = 'card-divider'>
+   <a id='card-title-${i}' href= '#'></a>
+   </div>
+   <img src='./assets/cook.jpg' id='thumb-${i}' style='height: 75px; width:75px; float: left;'>
+     <div class='card-section' style='float: right;'><a class = 'button save' id='${i}'>Save to Cookbook</a
+     ><p id='ing-${i}'></p>
+     </div>
+     </div>` */}
