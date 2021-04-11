e8699548584441025c4f0a92d71e41fc32b171de
John Ciancone <jackccinc@gmail.com>
2021-04-11 15:01:33 -0400

    Cookbook page added

---
+++ client/src/components/UserRecipe/UserRecipe.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const UserRecipe = () => {
+    const [name, setname] = useState("")
+    const[selectedFile, setSelectedFile]= useState(null);
+  return (
+    <div className="App">
+      <form>
+        <input type="text"
+         value={name}
+         onChange={(e) => setName (e.target.value)}
+          />
+
+        <input type="file"
+        value={selectedFile}
+        onChange={(e) => setSelectedFile(e.target.files[0])}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default UserRecipe;
---
+++ client/src/pages/Cookbook.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RecipeCard from "../components/RecipeCard";
+import UserRecipe from "../components/UserRecipe";
+
+
+function CookBook(){
+    return null;
+}
+
+export default CookBook
