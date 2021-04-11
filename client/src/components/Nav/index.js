6d2a82a9ff500c1f791d1d8dc58048243646df20
John Ciancone <jackccinc@gmail.com>
2021-04-11 16:24:35 -0400

    added navbar for app prototype

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,61 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+      <Link className="navbar-brand" to="/">
+        Cooking4Normals
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+          <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/home"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+              <Link 
+              to="/feed"
+              className={window.location.pathname ==="/feed" ? "nav-link active" : "nav-link"}
+              >Feed
+              </Link>
+              <Link 
+              to="/chat"
+              className={window.location.pathname ==="/chat" ? "nav-link active" : "nav-link"}
+              >Chat
+              </Link>
+              <Link 
+              to="/cookbook"
+              className={window.location.pathname ==="/cookbook" ? "nav-link active" : "nav-link"}
+              >Cookbook
+              </Link>
+              <Link 
+              to="/profile"
+              className={window.location.pathname ==="/profile" ? "nav-link active" : "nav-link"}
+              >Profile
+              </Link>
+              <Link 
+              to="/SearchResults"
+              className={window.location.pathname ==="/search" ? "nav-link active" : "nav-link"}
+              >Search Results
+              </Link>
+              <Link 
+              to="/video"
+              className={window.location.pathname ==="/video" ? "nav-link active" : "nav-link"}
+              >Videos
+              </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
