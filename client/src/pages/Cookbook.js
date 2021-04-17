29aeb822db3aeba5453910dfac1a42a244119001
gfraba <joshua.gagle@gmail.com>
2021-04-17 11:48:54 -0400

    cookbook changes, add saved recipe

--- client/src/pages/Cookbook.js
+++ client/src/pages/Cookbook.js
@@ -1,17 +1,33 @@
 import React from "react";
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipeCard from "../components/RecipeCard";
 import UserRecipe from "../components/UserRecipe";
+<<<<<<< Updated upstream
 import Footer from "../components/Footer/index";
+=======
+import Nav from "../components/Nav";
+import SearchBar from "../components/SearchBar";
+import Footer from "../components/Footer/index";
+import SavedRecipe from "../components/SavedRecipe";
+>>>>>>> Stashed changes
 
 function CookBook() {
   return (
     <>
-      <div className="bg">
-        <UserRecipe />
-        <Footer />
-      </div>
+        {/* <Nav /> */}
+        <div className="container mb-5 mt-5">
+            <h1 className="text-center">Personal Cookbook</h1>
+            <br></br>
+        
+        <SearchBar/>
+        <UserRecipe/>
+        <SavedRecipe/>
+        <Footer/>
+        </div>
+        
     </>
-  );
+    );
 }
 
 export default CookBook;
