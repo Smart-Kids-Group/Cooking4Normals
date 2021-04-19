e77a5e6aba0d8b6f5ea954e13d3dde45c69e6fe2
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-04-19 19:53:30 -0400

    fixed margin on cookbook page

--- client/src/components/RecipeImport/index.js
+++ client/src/components/RecipeImport/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button";
+import API from "../../utils/API";
 
 const RecipeImport = () => {
   
-  const handleInputChange = event => {}
-  const handleSubmit = () => {}
+  const [recipeURL, setRecipeURL] = useState("");
+
+  const handleInputChange = event => {
+    const { value } = event.target;
+    setRecipeURL(value);
+
+  }
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (recipeURL) {
+      API.importRecipeInfo(recipeURL).then((res) => {
+        let recipe = res.data[0];
+        API.saveRecipe(recipe)
+          .then((res) => console.log(res, recipe))
+          .catch((err) => console.log(err));
+      }); 
+    }
+  }
 
   return(
     <Container>
