a9912ffeb123836445d6b26a7cf18576a120badc
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-04-19 17:14:06 -0400

    added form to import recipes

--- client/src/components/RecipeImport/index.js
+++ client/src/components/RecipeImport/index.js
@@ -1,13 +1,26 @@
 import React from "react";
-import { Input, FormBtn } from "../Form";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button";
 
 const RecipeImport = () => {
-  return (
-    <div> To import a recipe from anywhere on the web, paste it here! </div>
+  
+  const handleInputChange = event => {}
+  const handleSubmit = () => {}
+
+  return(
+    <Container>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>To import a recipe from anywhere on the web, paste it here! </Form.Label>
+          <Form.Control type="text" placeholder="Paste your link here" onChange={handleInputChange}/>
+        </Form.Group>
+        <Button variant="primary" type="submit" onSubmit={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  
   )
 }
 
--- client/src/pages/Cookbook.js
+++ client/src/pages/Cookbook.js
@@ -1,10 +1,13 @@
 import React, { useContext, useEffect } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import UserRecipe from "../components/UserRecipe";
 import Footer from "../components/Footer";
 import RecipeList from "../components/RecipelList"
 import Container from "react-bootstrap/Container"
 import API from "../utils/API";
 import RecipeContext from "../utils/RecipeContext";
+import RecipeImport from "../components/RecipeImport";
 
  function CookBook() {
   // const [showUserRecipe, setShowUserRecipe] = useState(false);
@@ -25,16 +28,25 @@ import RecipeContext from "../utils/RecipeContext";
 
   
 return (
-    <>
+    
         <Container className="bg" absolute>
+          <Row>
+            <Col sm={4}>
+             <RecipeImport />
+             <UserRecipe />
+            </Col>
+            <Col sm={8}>
+        <Container >
           <h1 className="text-center">Personal Cookbook</h1>
           <br></br>
 
-          <UserRecipe />
+          
           <RecipeList data={recipes}/>
         </Container>
+        </Col>
+        </Row>
         <Footer />
-    </>
+        </Container>
   );
 }
 
