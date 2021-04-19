750767bc91940ebab88760420e3b8ece254f2b3a
Mike Voynovich <mvoyno@gmail.com>
2021-04-19 19:19:16 -0400

    merge conflicts6

--- client/src/components/VideoFeed/index.js
+++ client/src/components/VideoFeed/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import Thumbnail from "../Thumbnail";
 import { Container, Row, Col } from "../Grid";
+import YouTube from 'react-youtube';
 
+const VideoFeed = () => {
 
- const VideoFeed = () => {
-  return(
-    <h1>This is the list of videos</h1>
-  )
+class Example extends React.Component {
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        https:"developers.google.com/youtube/player_parameters",
+        autoplay: 1,
+      },
+    };
+
+    return <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />;
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
 }
+
+}
+
+
+//   return(
+//     <h1>This is the list of videos</h1>
+//   )
+// }
 
 export default VideoFeed;
--- client/src/pages/Video.js
+++ client/src/pages/Video.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import VideoSearch from "../components/VideoSearch";
 import VideoFeed from "../components/VideoFeed";
 import Footer from "../components/Footer/index";
+import API from "../utils/API";
 
 function Video() {
+
+  const [videos, setVideos] = useState([]);
+  const [videoSearch, setVideoSearch] = useState("bread");
+
+  useEffect(() => {
+    loadVideos()
+  }, [])
+
+  function loadVideos() {
+    API.searchVideos(videoSearch)
+      .then(res => 
+        {setVideos(res.items);
+        console.log(videos)}
+      )
+      .catch(err => console.log(err));
+  };
+
+  function handleInputChange(event) {
+    const { value } = event.target;
+       setVideoSearch(value)
+};
+
+  function handleSearchSubmit(event){
+    event.preventDefault();
+   
+      loadVideos();
+    };
+
   return (
     <>
       <div className="bg">
-        <VideoSearch />
-        <VideoFeed />
+      <VideoSearch handleInputChange= {handleInputChange}        handleSearchSubmit= {handleSearchSubmit}/>
+    <VideoFeed />
         <Footer />
       </div>
     </>
