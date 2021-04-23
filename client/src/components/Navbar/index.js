fa44af0a56c53dc52857711ec52de0d1cacc97db
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-04-23 08:38:57 -0400

    updated UserRecipe to open a Modal

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Cooking4Normals
-            <i class="fas fa-pepper-hot"></i>
+            <i className="fas fa-pepper-hot"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
